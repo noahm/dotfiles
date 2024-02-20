453d99a5ae530671fc152d93221dc56e8a54e49e
Noah Manneschmidt <noah@manneschmidt.net>
2024-02-20 08:59:15 -0800

    remove aea flag

--- fish/functions/mwauth.fish
+++ fish/functions/mwauth.fish
@@ -1,3 +1,3 @@
-function mwauth --wraps='op read "op://personal/Amazon Midway/password" | mwinit --aea' --description 'Auth to midway, automatically sourcing the PIN from 1Password'
-  op read "op://personal/Amazon Midway/password" | mwinit --aea $argv; 
+function mwauth --wraps='op read "op://personal/Amazon Midway/password" | mwinit' --description 'Auth to midway, automatically sourcing the PIN from 1Password'
+  op read "op://personal/Amazon Midway/password" | mwinit $argv;
 end
